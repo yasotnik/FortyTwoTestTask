c4434546c14fd6d834d89143ef860137b76a9ebd
yasotnik <sotnik.edu@yandex.com>
2017-03-04 00:23:39 +0200

    ticket:1  Redesigned test for view returning hardcoded data for the template

--- apps/hello/tests.py
+++ apps/hello/tests.py
@@ -1,10 +1,18 @@
 from django.test import TestCase
-from django.urls import reverse
+from django.core.urlresolvers import reverse
 
 
 class ShowBioTest(TestCase):
 
     def test_index_bio_template(self):
-        response = self.client.get(reverse('hello:index_bio'))
+        response = self.client.get(reverse('index_bio'))
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'templates/index.html')
+        self.assertTemplateUsed(response, 'hello/index.html', 'hello/base.html')
+
+    def test_index_bio_hardcoded_data(self):
+        response = self.client.get(reverse('index_bio'))
+        self.assertContains(response, 'Name')
+        self.assertContains(response, 'Email')
+        self.assertContains(response, 'Surname')
+        self.assertContains(response, 'JID')
+        self.assertContains(response, 'Skype')
