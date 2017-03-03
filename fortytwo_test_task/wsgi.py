bda2ff52bcfca08ec781a986a74df390b781b12a
yasotnik <sotnik.edu@yandex.com>
2017-03-03 16:27:47 +0200

    Fixed flake8

--- fortytwo_test_task/wsgi.py
+++ fortytwo_test_task/wsgi.py
@@ -9,6 +9,8 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 
 import os
 from django.core.wsgi import get_wsgi_application
+
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fortytwo_test_task.settings")
 
 application = get_wsgi_application()
