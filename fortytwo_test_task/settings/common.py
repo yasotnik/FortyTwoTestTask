71d5cf6ef7bdef0dd930a4aba826fd31413e2e7a
yasotnik <sotnik.edu@yandex.com>
2017-03-03 16:14:30 +0200

    Changed DB name

--- fortytwo_test_task/settings/common.py
+++ fortytwo_test_task/settings/common.py
@@ -65,7 +65,7 @@ WSGI_APPLICATION = 'fortytwo_test_task.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'dbcups.sqlite3'),
     }
 }
 
